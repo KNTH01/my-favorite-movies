91d5d5fbf8e09f708fcebcca12e932e21302bbd5
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2016-11-05 21:05:35 +0100

    rename movie-db to movieOMDB

--- test/movies-omdb.test.js
+++ test/movies-omdb.test.js
@@ -1,11 +1,11 @@
 const chai = require('chai')
 const expect = chai.expect
 
-describe('MovieDB search', function () {
+describe('omdb API movies search', function () {
   it('should make a search', function (done) {
     let searchMovie = 'fast'
 
-    MovieDB.search(searchMovie).then(result => {
+    MoviesOMDB.search(searchMovie).then(result => {
       expect(result.Search).to.be.a('array')
       expect(result.totalResults).to.be.above(0)
       expect(result.Response).to.be.equal('True')
@@ -16,7 +16,7 @@ describe('MovieDB search', function () {
   it('should have coherent search results', function (done) {
     let searchMovie = 'Warcraft'
 
-    MovieDB.search(searchMovie).then(result => {
+    MoviesOMDB.search(searchMovie).then(result => {
       expect(result).to.be.a('array')
       done()
     })
