78d03bbb473d08f3ba79009855af70658e2808e0
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2016-11-05 22:49:39 +0100

    add routes

---
+++ app/routes/movies.js
@@ -0,0 +1,65 @@
+const Movie = require('../models/Movie')
+
+function getMovies (req, res) {
+  Movie
+    .find({})
+    .exec((err, movies) => {
+      if (err) {
+        res.send(err)
+      }
+
+      res.json(movies)
+    })
+}
+
+function postMovie (req, res) {
+  const newMovie = new Movie(req.body)
+
+  newMovie.save((err, movie) => {
+    if (err) {
+      res.send(err)
+    }
+
+    res.json({
+      message: 'Movie successfully added!',
+      movie
+    })
+  })
+}
+
+function getMovie (req, res) {
+  Movie.findById(req.params.id, (err, movie) => {
+    if (err) {
+      res.send(err)
+    }
+
+    res.json(movie)
+  })
+}
+
+function deleteMovie (req, res) {
+  Movie.remove({_id: req.params.id}, (err, result) => {
+    if (err) {
+      res.send(err)
+    }
+
+    res.json({ message: 'Movie successfully deleted!', result })
+  })
+}
+
+function updateMovie (req, res) {
+  Movie.findById({_id: req.params.id}, (err, movie) => {
+    if (err) {
+      res.send(err)
+    }
+
+    Object.assign(movie, req.body).save((err, movie) => {
+      if (err) {
+        res.send(err)
+      }
+      res.json({ message: 'Movie updated!', movie })
+    })
+  })
+}
+
+module.exports = { getMovies, postMovie, getMovie, deleteMovie, updateMovie }
