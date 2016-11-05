5489cc7d3a85ba82fc88582e89c7d39a6c717771
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2016-11-05 22:35:59 +0100

    add mongo / mongoose and conf file

--- app/index.js
+++ app/index.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
 const nunjucks = require('nunjucks')
+const mongoose = require('mongoose')
+const config = process.env.NODE_ENV === 'test' ? require('../config/test.json') : require('../config/default.json')
 
+// configure db connection
+mongoose.connect(config.dbHost)
+
+// configure view
 nunjucks.configure(`${__dirname}/views`, {
   autoescape: true,
   watch: true, // should be true in dev mode only
   express: app
 })
 
+// configure public assets
 app.use('/assets', express.static('public'))
 
 // set up bodyParser middleware
