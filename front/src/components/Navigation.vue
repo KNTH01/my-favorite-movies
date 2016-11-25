e08f76e4b599821751bc1116206080bc552be7c5
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2016-11-25 15:36:52 +0100

    active routing

--- front/src/components/Navigation.vue
+++ front/src/components/Navigation.vue
@@ -5,8 +5,8 @@
         <router-link class="navbar-brand" tag="a" to="/">My Favorite Movie</router-link>
       </div>
       <ul class="nav navbar-nav">
-        <router-link tag="li" to="/"><a>Home</a></router-link>
-        <router-link tag="li" to="/search/movies"><a>Search Movies</a></router-link>
+        <router-link tag="li" active-class="active" to="/" exact><a>Home</a></router-link>
+        <router-link tag="li" active-class="active" to="/search/movies"><a>Search Movies</a></router-link>
       </ul>
     </div>
   </nav>
